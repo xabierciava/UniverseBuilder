f52e1c6a5b1a6233c3c45a87f96a9d186d99baa0
xabier cia <71944003+xabierciava@users.noreply.github.com>
2021-03-20 14:12:20 +0100

    Remodelado de interfaces, ahora usan material.io y los errores se comunican correctamente

--- app/src/main/java/com/example/universebuilder/Entrar.java
+++ app/src/main/java/com/example/universebuilder/Entrar.java
@@ -10,6 +10,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.regex.Pattern;
+
 import api.ApiInterface;
 import api.ServiceGenerator;
 import model.PaqueteUsuario;
@@ -20,13 +22,26 @@ import retrofit2.Response;
 
 public class Entrar extends AppCompatActivity {
 
+    EditText email;
+    EditText psw;
+
+    public final Pattern EMAIL_ADDRESS_PATTERN = Pattern.compile(
+            "[a-zA-Z0-9+._%-+]{1,256}" +
+                    "@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9-]{0,64}" +
+                    "(" +
+                    "." +
+                    "[a-zA-Z0-9][a-zA-Z0-9-]{0,25}" +
+                    ")+"
+    );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrar);
         TextView volver2 = (TextView)findViewById(R.id.volver_atras2);
-        EditText email = (EditText)findViewById(R.id.textInputEditTextEmailEntrar);
-        EditText psw = (EditText)findViewById(R.id.textInputEditTextPassEntrar);
+        email = (EditText)findViewById(R.id.textInputEditTextEmailEntrar);
+        psw = (EditText)findViewById(R.id.textInputEditTextPassEntrar);
         Button enviar = (Button)findViewById(R.id.botonInternoEntrar);
         volver2.setOnClickListener(v -> {
             finish();
@@ -36,31 +51,37 @@ public class Entrar extends AppCompatActivity {
         enviar.setOnClickListener(v -> enviarDatos(email.getText().toString(),psw.getText().toString()));
     }
 
-    private void enviarDatos(String email,String psw){
-        ApiInterface apiInterface = ServiceGenerator.createService(ApiInterface.class);
-        Call<PaqueteUsuario> call=apiInterface.getUsuario(email,psw);
-        call.enqueue(new Callback<PaqueteUsuario>() {
+    private void enviarDatos(String emailStr,String psw){
+        //Si el email no tiene formato email
+        if(!EMAIL_ADDRESS_PATTERN.matcher(emailStr).matches()){
+            email.setError("No es un correo electrónico");
+        }else{
+            email.setError(null);
+            ApiInterface apiInterface = ServiceGenerator.createService(ApiInterface.class);
+            Call<PaqueteUsuario> call = apiInterface.getUsuario(emailStr, psw);
+            call.enqueue(new Callback<PaqueteUsuario>() {
 
-            @Override
-            public void onResponse(Call<PaqueteUsuario> call, Response<PaqueteUsuario> response) {
-                System.out.println("Success");
-                String status = response.body().getStatus();
-                if(status.equals("OK")){
-                    Usuario user = response.body().getData();
-                    Intent intent = new Intent(Entrar.this, menuPrincipal.class);
-                    startActivity(intent);
-                }else{
-                    Toast toast = Toast.makeText(Entrar.this, "No existe este usuario", Toast.LENGTH_LONG);
-                    toast.show();
-                    System.out.println("USUARIO INEXISTENTE");
+                @Override
+                public void onResponse(Call<PaqueteUsuario> call, Response<PaqueteUsuario> response) {
+                    System.out.println("Success");
+                    String status = response.body().getStatus();
+                    if (status.equals("OK")) {
+                        Usuario user = response.body().getData();
+                        Intent intent = new Intent(Entrar.this, menuPrincipal.class);
+                        startActivity(intent);
+                    } else {
+                        Toast toast = Toast.makeText(Entrar.this, "No existe este usuario", Toast.LENGTH_LONG);
+                        toast.show();
+                        System.out.println("USUARIO INEXISTENTE");
+                    }
                 }
-            }
 
-            @Override
-            public void onFailure(Call<PaqueteUsuario> call, Throwable t) {
-                Log.e("tag", t.getMessage());
-            }
-        });
+                @Override
+                public void onFailure(Call<PaqueteUsuario> call, Throwable t) {
+                    Log.e("tag", t.getMessage());
+                }
+            });
+        }
     }
 
 }
--- app/src/main/java/com/example/universebuilder/Registrar.java
+++ app/src/main/java/com/example/universebuilder/Registrar.java
@@ -10,6 +10,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.regex.Pattern;
+
 import api.ApiInterface;
 import api.ServiceGenerator;
 import retrofit2.Call;
@@ -18,15 +22,28 @@ import retrofit2.Response;
 
 public class Registrar extends AppCompatActivity {
 
+    EditText nombre;
+    EditText email;
+    EditText psw1;
+    EditText psw2;
+    TextInputLayout layPsw;
+    TextInputLayout layEmail;
+
+    String regex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+
+    public final Pattern EMAIL_ADDRESS_PATTERN = Pattern.compile(regex);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
         TextView volver = (TextView)findViewById(R.id.volver_atras);
-        EditText nombre = (EditText)findViewById(R.id.editTextNombreRegistrar);
-        EditText psw1 = (EditText)findViewById(R.id.editTextPasswordRegistrar);
-        EditText psw2 = (EditText)findViewById(R.id.editTextPassword2Registrar);
-        EditText email = (EditText)findViewById(R.id.editTextEmailRegistrar);
+        nombre = (EditText)findViewById(R.id.textInputEditTextNombreRegistrar);
+        psw1 = (EditText)findViewById(R.id.textInputEditTextPassRegistrar);
+        psw2 = findViewById(R.id.textInputEditTextPass2Registrar);
+        layPsw = findViewById(R.id.outlinedTextFieldPass2Registrar);
+        email = (EditText)findViewById(R.id.textInputEditTextEmailRegistrar);
+        layEmail = findViewById(R.id.outlinedTextFieldEmailRegistrar);
         Button enviar = (Button)findViewById(R.id.botonInternoRegistrar);
 
         volver.setOnClickListener(v -> {
@@ -37,42 +54,48 @@ public class Registrar extends AppCompatActivity {
         });
     }
 
-    private void enviarDatos(String nombre,String psw,String psw2,String email){
-        if (checkPsw(psw,psw2)){
-            //enviar datos a la base de datos
-            ApiInterface apiInterface = ServiceGenerator.createService(ApiInterface.class);
-            Call<String> call=apiInterface.insertaUsuario(nombre,psw,email);
-            call.enqueue(new Callback<String>() {
-                @Override
-                public void onResponse(Call<String> call, Response<String> response) {
-                    if(response.isSuccessful()){
-                        System.out.println("Success");
-                        if(response.body().equals("-1")){
-                            Toast toast = Toast.makeText(getApplicationContext(), "Ya existe una cuenta con este correo", Toast.LENGTH_LONG);
-                            toast.show();
-                            System.out.println("ya existe este correo");
-                        }else{
-                            Intent intent = new Intent(Registrar.this, menuPrincipal.class);
-                            startActivity(intent);
+    private void enviarDatos(String nombreStr,String pswStr,String psw2Str,String emailStr){
+        if(EMAIL_ADDRESS_PATTERN.matcher(emailStr).matches()) {
+            layEmail.setError(null);
+            if (checkPsw(pswStr, psw2Str)) {
+                layPsw.setError(null);
+                //enviar datos a la base de datos
+                ApiInterface apiInterface = ServiceGenerator.createService(ApiInterface.class);
+                Call<String> call = apiInterface.insertaUsuario(nombreStr, pswStr, emailStr);
+                call.enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        if (response.isSuccessful()) {
+                            System.out.println("Success");
+                            if (response.body().equals("-1")) {
+                                layEmail.setError("Ya existe una cuenta con este correo");
+                                System.out.println("ya existe este correo");
+                            } else {
+                                Intent intent = new Intent(Registrar.this, menuPrincipal.class);
+                                startActivity(intent);
+                            }
+
                         }
-
                     }
-                }
 
-                @Override
-                public void onFailure(Call<String> call, Throwable t) {
-                    Log.e("tag", t.getMessage());
-                }
-            });
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+                        Log.e("tag", t.getMessage());
+                    }
+                });
 
+            } else {
+                layPsw.setError("Las contraseñas no coinciden");
+                Toast toast = Toast.makeText(getApplicationContext(), "Las contraseñas no coinciden", Toast.LENGTH_LONG);
+                toast.show();
+            }
         }else{
-            Toast toast = Toast.makeText(getApplicationContext(), "Las contraseñas no coinciden", Toast.LENGTH_LONG);
-            toast.show();
+            layEmail.setError("No es un correo electrónico");
         }
     }
 
-    public Boolean checkPsw(String psw1,String psw2){
-        if(psw1.equals(psw2)){
+    public Boolean checkPsw(String psw1Str,String psw2Str){
+        if(psw1Str.equals(psw2Str)){
             return true;
         }else{
             return false;
